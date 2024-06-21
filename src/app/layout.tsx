76387e80a8e5998d336ceda9f190cd22660cff95
AbdulazizRus <coderdim@gmail.com>
2024-06-21 22:34:30 +0500

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,31 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/Navbar'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Abdulaziz Social Media App",
-  description: "Social media app built with Next.js",
-};
+  title: 'Nora Social Media App',
+  description: 'Social media app built with Next.js',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>test{children}</body>
+      <body className={inter.className}>
+        <div className="w-full px-4 bg-white md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+          <Navbar />
+        </div>
+        <div className="bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+          {children}
+        </div>
+      </body>
     </html>
-  );
+  )
 }
+//19:00
