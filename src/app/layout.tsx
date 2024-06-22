3befe3e9bfda2eb834992d04e757438d73a7d701
AbdulazizRus <coderdim@gmail.com>
2024-06-22 12:24:55 +0500

    auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 
@@ -16,6 +23,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
         <div className="w-full px-4 bg-white md:px-8 lg:px-16 xl:px-32 2xl:px-64">
@@ -26,6 +34,7 @@ export default function RootLayout({
         </div>
       </body>
     </html>
+    </ClerkProvider>
   )
 }
 //19:00
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,14 @@
 import Link from 'next/link'
 import MobileMenu from './MobileMenu'
 import Image from 'next/image'
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { CircleUserRound } from 'lucide-react'
 
 const Navbar = () => {
   return (
@@ -15,22 +23,69 @@ const Navbar = () => {
       <div className="hidden md:flex w-[50%] text-sm">
         <div className="flex gap-6 text-gray-600">
           <Link href="/" className="flex items-center gap-2">
-            <Image src="/home.png" alt="Home" width={16} className='w-4 h-4' height={16} />
+            <Image
+              src="/home.png"
+              alt="Home"
+              width={16}
+              className="w-4 h-4"
+              height={16}
+            />
             <span>Homepage</span>
           </Link>
           <Link href="/" className="flex items-center gap-2">
-            <Image src="/friends.png" alt="Friends" width={16} className='w-4 h-4' height={16} />
+            <Image
+              src="/friends.png"
+              alt="Friends"
+              width={16}
+              className="w-4 h-4"
+              height={16}
+            />
             <span>Friends</span>
           </Link>
           <Link href="/" className="flex items-center gap-2">
-            <Image src="/stories.png" alt="Stories" width={16} className='w-4 h-4' height={16} />
+            <Image
+              src="/stories.png"
+              alt="Stories"
+              width={16}
+              className="w-4 h-4"
+              height={16}
+            />
             <span>Stories</span>
           </Link>
         </div>
       </div>
       {/* right */}
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
-
+        <ClerkLoading>
+          <div
+            className="inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            role="status"
+          >
+            {/* <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span> */}
+          </div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="cursor-pointer">
+              <Image src="/people.png" alt="" width={20} height={20} />
+            </div>
+            <div className="cursor-pointer">
+              <Image src="/messages.png" alt="" width={20} height={20} />
+            </div>
+            <div className="cursor-pointer">
+              <Image src="/notifications.png" alt="" width={20} height={20} />
+            </div>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <div className="flex items-center gap-2 text-sm">
+              <CircleUserRound width={20} height={20} />
+              <Link href="/sign-in">Login/Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
         <MobileMenu />
       </div>
     </div>
