4974e766efb153bc3ace2caa6f5320346b1695c0
AbdulazizRus <coderdim@gmail.com>
2024-06-22 23:37:15 +0500

    birthdayss

---
+++ src/components/Ad.tsx
@@ -0,0 +1,7 @@
+const Ad = ({size}: {size: "sm" | "md" | "lg"}) => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm">Ad</div>
+  )
+}
+
+export default Ad
---
+++ src/components/Birthdays.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Birthdays = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      {/* top */}
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">Birthdays</span>
+      </div>
+      {/* user */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src="https://images.pexels.com/photos/19915666/pexels-photo-19915666.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={40}
+            height={40}
+            className="w-10 h-10 rounded-full object-cover"
+          />
+          <span className="font-semibold">Wayne Burton</span>
+        </div>
+        <div className="flex gap-3 justify-end">
+          <button className="bg-blue-500 text-white text-xs px-2 py-1 rounded-md">
+            Celebrate
+          </button>
+        </div>
+      </div>
+      {/* upcoming */}
+      <div className="p-4 bg-slate-100 rounded-lg flex items-center gap-4">
+        <Image
+          src="/gift.png"
+          alt=""
+          width={24}
+          height={24}
+        />
+        <Link href="/" className='flex flex-col gap-1 text-xs'>
+            <span className='text-gray-700 font-semibold'>
+              Upcoming Birthdays
+            </span>
+            <span className="text-gray-500">
+                See other 16 have upcoming birthdays
+            </span>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Birthdays
