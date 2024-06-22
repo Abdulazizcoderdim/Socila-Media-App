929d54b5cf622092208c7fcdeabcae59550fad82
AbdulazizRus <coderdim@gmail.com>
2024-06-22 12:34:35 +0500

    home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
         </Link>
       </div>
       {/* cenet */}
-      <div className="hidden md:flex w-[50%] text-sm">
+      <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
         <div className="flex gap-6 text-gray-600">
           <Link href="/" className="flex items-center gap-2">
             <Image
@@ -53,6 +53,14 @@ const Navbar = () => {
             <span>Stories</span>
           </Link>
         </div>
+        <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent outline-none"
+          />
+          <Image src="/search.png" alt="search" width={14} height={14} />
+        </div>
       </div>
       {/* right */}
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
@@ -68,13 +76,13 @@ const Navbar = () => {
         </ClerkLoading>
         <ClerkLoaded>
           <SignedIn>
-            <div className="cursor-pointer">
-              <Image src="/people.png" alt="" width={20} height={20} />
+            <div className="cursor-pointer sm:flex hidden">
+              <Image src="/people.png" alt="" width={24} height={24} />
             </div>
-            <div className="cursor-pointer">
+            <div className="cursor-pointer sm:flex hidden">
               <Image src="/messages.png" alt="" width={20} height={20} />
             </div>
-            <div className="cursor-pointer">
+            <div className="cursor-pointer sm:flex hidden">
               <Image src="/notifications.png" alt="" width={20} height={20} />
             </div>
             <UserButton />
