bb5438742f92a9cc2e86d1032cd1a495cfd9af86
AbdulazizRus <coderdim@gmail.com>
2024-06-22 11:53:30 +0500

    auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,41 @@
+import Link from 'next/link'
+import MobileMenu from './MobileMenu'
+import Image from 'next/image'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex items-center justify-between h-24">
+      {/* left */}
+      <div className="md:hidden lg:block w-[20%]">
+        <Link href="/" className="font-bold text-xl text-blue-600">
+          NORASOCIAL
+        </Link>
+      </div>
+      {/* cenet */}
+      <div className="hidden md:flex w-[50%] text-sm">
+        <div className="flex gap-6 text-gray-600">
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/home.png" alt="Home" width={16} className='w-4 h-4' height={16} />
+            <span>Homepage</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/friends.png" alt="Friends" width={16} className='w-4 h-4' height={16} />
+            <span>Friends</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/stories.png" alt="Stories" width={16} className='w-4 h-4' height={16} />
+            <span>Stories</span>
+          </Link>
+        </div>
+      </div>
+      {/* right */}
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+
+        <MobileMenu />
+      </div>
+    </div>
   )
 }
+//33:00
 
 export default Navbar
