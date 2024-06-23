0c3074bcb9367704822af79fd43de9c8b4aaebee
AbdulazizRus <coderdim@gmail.com>
2024-06-23 13:23:29 +0500

    home page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,21 +1,54 @@
 import Feed from '@/components/Feed'
 import LeftMenu from '@/components/LeftMenu'
 import RightMenu from '@/components/RightMenu'
+import Image from 'next/image'
 import React from 'react'
 
 const ProfilePage = () => {
   return (
     <div className="flex gap-6 pt-6">
       <div className="hidden xl:block w-[20%]">
-        <LeftMenu />
+        <LeftMenu type="profile" />
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image
+                src="https://images.pexels.com/photos/25004848/pexels-photo-25004848.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+                fill
+                className="rounded-md object-cover"
+              />
+              <Image
+                src="https://images.pexels.com/photos/20876214/pexels-photo-20876214.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
+              />
+            </div>
+            <h1 className='mt-20 mb-4 text-2xl font-medium'>Elva Weaver</h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className='font-medium'>123</span>
+                <span className='text-sm'>Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className='font-medium'>1.2K</span>
+                <span className='text-sm'>Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className='font-medium'>1.4K</span>
+                <span className='text-sm'>Following</span>
+              </div>
+            </div>
+          </div>
           <Feed />
         </div>
       </div>
       <div className="hidden lg:block w-[30%]">
-        <RightMenu userId='test' />
+        <RightMenu userId="test" />
       </div>
     </div>
   )
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,57 @@
+import Image from 'next/image'
+
+const ProfileCard = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
+      <div className="h-20 relative">
+        <Image
+          src="https://images.pexels.com/photos/24879609/pexels-photo-24879609.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+          fill
+          className="rounded-md object-cover"
+        />
+        <Image
+          src="https://images.pexels.com/photos/8681433/pexels-photo-8681433.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+          width={48}
+          height={48}
+          className="rounded-full w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10"
+        />
+      </div>
+      <div className="min-h-20 flex flex-col gap-2 items-center">
+        <span className="font-semibold">Edward Gabriel May</span>
+        <div className="flex items-center gap-4">
+          <div className="flex">
+            <Image
+              src="https://images.pexels.com/photos/8681433/pexels-photo-8681433.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full w-3 h-3"
+            />
+            <Image
+              src="https://images.pexels.com/photos/8681433/pexels-photo-8681433.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full w-3 h-3"
+            />
+            <Image
+              src="https://images.pexels.com/photos/8681433/pexels-photo-8681433.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full w-3 h-3"
+            />
+          </div>
+          <span className="text-xs text-gray-500">500 Followers</span>
+        </div>
+        <button className="bg-blue-500 text-white text-xs p-2 rounded-md">
+          My Profile
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileCard
