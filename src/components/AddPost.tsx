d2cf0dc8892d6189a98ae6214058444886a3c4fc
AbdulazizRus <coderdim@gmail.com>
2024-06-25 10:51:11 +0500

    ngrok

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,6 +1,13 @@
+import prisma from '@/lib/client'
+import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
+import { ObjectId } from 'mongodb'
+
+const id = new ObjectId().toString()
 
 const AddPost = () => {
+  const { userId } = auth()
+  
   return (
     <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
       {/* Avatart */}
@@ -14,10 +21,11 @@ const AddPost = () => {
       {/* Post */}
       <div className="flex-1">
         {/* Text input */}
-        <div className="flex gap-4">
+        <form action="" className="flex gap-4">
           <textarea
             className="flex-1 bg-slate-100 rounded-t rounded-l-lg p-2"
             placeholder="What's on your mind?"
+            name="desc"
           ></textarea>
           <Image
             width={20}
@@ -26,7 +34,8 @@ const AddPost = () => {
             alt=""
             className="w-5 h-5 cursor-pointer self-end"
           />
-        </div>
+          <button>Send</button>
+        </form>
         {/* Post options */}
         <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
           <div className="flex items-center gap-2 cursor-pointer">
@@ -52,3 +61,4 @@ const AddPost = () => {
 }
 
 export default AddPost
+//2:32:00
