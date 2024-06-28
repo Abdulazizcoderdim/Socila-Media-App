ef4e23d0c285fd5987e4a43ffb114ecab1622fcd
AbdulazizRus <coderdim@gmail.com>
2024-06-28 09:18:12 +0500

    chanegs

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -292,3 +292,39 @@ export const addPost = async (formData: FormData, img: string) => {
     console.log(err);
   }
 };
+
+export const addStory = async (img: string) => {
+  const { userId } = auth();
+
+  if (!userId) throw new Error("User is not authenticated!");
+
+  try {
+    const existingStory = await prisma.story.findFirst({
+      where: {
+        userId,
+      },
+    });
+
+    if (existingStory) {
+      await prisma.story.delete({
+        where: {
+          id: existingStory.id,
+        },
+      });
+    }
+    const createdStory = await prisma.story.create({
+      data: {
+        userId,
+        img,
+        expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    return createdStory;
+  } catch (err) {
+    console.log(err);
+  }
+};
